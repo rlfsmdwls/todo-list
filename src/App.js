62c4e26ac6f01e32e019d5798bf27c8d89375d3e
gilneungjin <kilnj@smartauth.kr>
2018-06-15 12:49:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/js/TodoListTemplate';
+import Form from './components/js/Form';
+import TodoItemList from './components/js/TodoItemList';
+import Palette from './components/js/Palette';
+
+const colors = ['#343a40','#f03e3e','#12b886','#228ae6'];
+
+class App extends Component {
+
+  id = 3
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: ' 리액트 시작', checked: false},
+      { id: 1, text: ' JSX 사용', checked: true},
+      { id: 2, text: ' 라이프 사이클 이해', checked: false}
+    ],
+    color: '#343a40'
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos, color } = this.state;
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false,
+        color
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index];
+
+    const nextTodos = [...todos];
+
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  handleSelectColor = (color) => {
+    this.setState({
+      color
+    })
+  }
+
+  render() {
+    const { input, todos, color } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+      handleSelectColor
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+          color={color}
+        />
+      )}
+        palette={(
+          <Palette colors={colors} selected={color} onSelect={handleSelectColor}/>
+        )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
